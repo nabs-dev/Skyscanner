621da62b10acb320f729c78bb93e7c8476cc34b3
nabs-dev <nabihakashif631@gmail.com>
2025-07-30 00:06:55 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = "localhost"; // Adjust if your host differs
+$dbname = "dbfsv39if6njjl";
+$username = "u8gr0sjr9p4p4";
+$password = "9yxuqyo3mt85";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
